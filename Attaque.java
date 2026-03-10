77927191ef820580ffcaf96e87756f43ffada667
othmane <othmane.azizi05@gmail.com>
2026-03-10 14:49:06 +0100

    Validation

---
+++ Attaque.java
@@ -0,0 +1,6 @@
+public class Attaque {
+    int a =0;
+    public Attaque(){
+        this.a = 1;
+    }
+}
---
+++ Pokemon.java
@@ -0,0 +1,36 @@
+public class Pokemon {
+    private static final int NB_ATTAQUES = 4;
+    private String nom;
+    private int pvMax;
+    private int pv;
+    private int attaque;
+    private int defense;
+    private int vitesse;
+    private Type type1;
+    private Type type2;
+    private Attaque[] attaques;
+
+
+    public Pokemon(String nom){
+        this.nom = nom;
+        this.attaques = new Attaque[NB_ATTAQUES];
+        for(int i=0; i<this.NB_ATTAQUES;i++){
+            this.attaques[i] = new Attaque(); //A compléter 
+        }
+    }
+
+    public void getAttaqueByIndex(int index){
+        this.attaque = attaque;
+    }
+    
+    public attaquer(Attaque[], ){}
+
+    public int getPvMax(){
+        return this.pvMax;
+    }
+
+    public void setPvMax(int pvMax){
+        this.pvMax = pvMax;
+    }
+
+}
