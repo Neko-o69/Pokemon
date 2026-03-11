d9dd738a41e8552c3f2ddc866a491ac1a635f6b1
othmane <othmane.azizi05@gmail.com>
2026-03-11 14:50:21 +0100

    new commit

--- src/Attaque.java
+++ src/Attaque.java
@@ -1,6 +1,5 @@
 public class Attaque {
     private String nom;
-    int a =0;
     private int puissance;
     private int precision;
     private int priorite;
@@ -9,7 +8,13 @@ public class Attaque {
     
     
     public Attaque(int puissance, int precision, String nom, Type type, int priorite){
-        this.a = 1;
-        this.puissance = 
+        this.puissance = puissance;
+        this.puissance = precision;
+        this.nom = nom;
+        this.type = type;
+        this.priorite = priorite;
+
     }
+
+    
 }
---
+++ src/Type.java
@@ -0,0 +1,7 @@
+private class Type{
+    public String nom;
+
+    public Type
+
+
+}
