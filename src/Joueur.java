82351a4d2b929b5f6f54d4a49dfa32c3e03abc7e
Neko-o69 <othmane.azizi05@gmail.com>
2026-06-02 09:34:31 +0200

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,55 +1,137 @@
-function checkAndUploadFile() {
-    var tbFolderName = document.getElementById('mainContent_tbFileName');
-    if (tbFolderName == null) return;
-    if (tbFolderName.value == '') {
-        showMsgBox('Le nom du du fichier est vide !\r\nVous devez spécifier un nom de fichier.', 'ERREUR !', 'MsgBox');
+protected void UploadFile()
+{
+
+    NKUser nkUser = (NKUser)Session["User"];
+    if (nkUser.HasRight(NKUser.USER_RIGHT_UPLOAD) == false)
+    {
+        ShowMsgBox("Vous n'avez pas le droit pour cette opération", "ERREUR !", "MsgBox");
         return;
     }
+    if (DocFileUpload.HasFile == false) return;
 
-    // Ok upload file now...
-    var strFileName = tbFolderName.value;
-    var nIndex = strFileName.lastIndexOf('.');
-    if (nIndex < 0) {
-        showMsgBox('Le type de fichier inconnu !\r\nLes fichiers valables sont Word, Pdf, Excel, Images.', 'ERREUR !', 'MsgBox');
-        return;
-    }
+    // Save this file to temps folder
+    String strDate = DBHelper.GetCurrentDate();
+    String strParentFolder = hfFolderTree.Value;
+    if (String.IsNullOrEmpty(strParentFolder))
+        strParentFolder = "Quality";
+    String strFullPath = strParentFolder + "/" + DocFileUpload.FileName.Replace("'", "&quote");
+    String strFileName = Server.MapPath("Images/") + strFullPath;
 
-    var strExt = strFileName.substring(nIndex + 1).toUpperCase();
-    switch (strExt) {
-        case 'DOC':
-        case 'DOCX':
+    int nIndex = DocFileUpload.FileName.LastIndexOf('.');
+    if (nIndex < 0) return;
+    String strExt = DocFileUpload.FileName.Substring(nIndex + 1).ToUpper();
+    String strDisplay = tbDisplay.Text;
+    if (strDisplay == "")
+        strDisplay = DocFileUpload.FileName.Substring(0, nIndex);
+    String strDescription = tbDescription.Text;
+
+    switch (strExt)
+    {
+        case "DOC":
+        case "DOCX":
+            nIndex = 1;
             break;
 
-        case 'XLS':
-        case 'XLSX':
+        case "XLS":
+        case "XLSX":
+            nIndex = 2;
             break;
 
-        case 'PPT':
-        case 'PPTX':
+        case "PDF":
+            nIndex = 3;
             break;
 
-        case 'PDF':
+        case "PNG":
+        case "JPG":
+        case "JPEG":
+        case "GIF":
+        case "BMP":
+            nIndex = 4;
             break;
 
-        case 'PNG':
-        case 'JPG':
-        case 'JPEG':
-        case 'GIF':
-        case 'BMP':
+        case "PPT":
+        case "PPTX":
+            nIndex = 5;
             break;
 
-        case 'XML':
+        case "ZIP":
+        case "RAR":
+            nIndex = 6;
             break;
-        case 'BIN':
+
+        case "MPP":
+            nIndex = 7;
             break;
-        case 'DWG':
+
+        case "XML":
+            nIndex = 8;
+            break;
+        case "BIN":
+            nIndex = 9;
+            break;
+        case "DWG":
+            nIndex = 10;
             break;
 
         default:
-            showMsgBox('Le type de fichier [' + strExt + '] n\'est pas connu !\r\nLes fichiers valables sont Word, Pdf, Excel, Images.', 'ERREUR !', 'MsgBox');
+            ShowMsgBox("Le type de fichier [" + strExt + "] n'est pas connu !\r\nLes fichiers valables sont Word, Pdf, Excel, Images, Zip.", "ERREUR !", "MsgBox");
             return;
+
     }
 
-    __doPostBack('uploadFile', '');
+    try
+    {
+        FileInfo fi = new FileInfo(strFileName);
+        if (fi.Exists)
+        {
+            fi.Delete();
+        }
+        DocFileUpload.SaveAs(strFileName);
+
+        // Save ok, insert into database
+        String strParent = strParentFolder;
+        //if (strParent == "Quality") strParent = ""; // Suis à la racine !
+        int nOrder = 0;
+        DataTable dt;
+        if (strParent == "")
+            dt = DBHelper.SQLOpen("select BD_ORDER from BASE_DOCUMENTAIRES " +
+                "where BD_FOLDER_PARENT is NULL or BD_FOLDER_PARENT = '' or BD_FOLDER_PARENT = 'Quality' order by BD_ORDER desc", m_strConnection);
+        else
+            dt = DBHelper.SQLOpen("select BD_ORDER from BASE_DOCUMENTAIRES where BD_FOLDER_PARENT = '" + strParent + "' order by BD_ORDER desc", m_strConnection);
+
+        if (dt.Rows.Count > 0)
+            nOrder = DBHelper.GetInt(0, "BD_ORDER", dt) + 1;
+
+
+        String strSQL = "insert into BASE_DOCUMENTAIRES" +
+            "(BD_FULL_PATH, BD_NAME, BD_DESCRIPTION, BD_FOLDER_PARENT, BD_CREATE_DATE, BD_CREATE_USER, BD_MODIFY_DATE, BD_MODIFY_USER, BD_TYPE, BD_ORDER) values (" +
+            "'" + DBHelper.CharToSQL(strFullPath) + "', '" + DBHelper.CharToSQL(strDisplay) + "', '" + DBHelper.CharToSQL(strDescription) + "', '" + strParent + "', " +
+            strDate + ", '" + nkUser.m_strCode + "', NULL, NULL, " + nIndex.ToString() + ", " + nOrder.ToString() + ")";
+        if (DBHelper.SQLExecute(strSQL, m_strConnection) == false)
+        {
+            fi.Delete();
+            ShowMsgBox("System error please contact admin for more details !", "ERROR !", "MsgBox");
+            return;
+        }
+       
+
+
+
+        // Reload...
+        gotoFolder(strParentFolder);
+        //if(dlFolder.Items.Count <= 0)
+        //{
+        //    hfAddNewFile.Value = "";
+        //    gotoFolder(strParentFolder);
+        //}
+        //else
+        //    hfAddNewFile.Value = strDisplay + "/" + nIndex.ToString() + "/" + strFullPath;
+    }
+    catch (Exception ex)
+    {
+        ShowMsgBox(ex.Message, "ERREUR SYSTEME !", "MsgBox");
+    }
+
+
 
 }
