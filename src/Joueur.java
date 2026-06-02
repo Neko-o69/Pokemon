da76e20f3a7b38dbb8f9e975b4779155f27f924d
Neko-o69 <othmane.azizi05@gmail.com>
2026-06-02 09:32:04 +0200

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,29 +1,55 @@
-Dropzone.js is a popular JavaScript library that provides an easy way to upload files in an ASP.NET MVC application. It uses HTML5’s drag and drop API and Ajax to improve the user experience of file uploading. To use Dropzone.js in your ASP.NET MVC application, follow these steps:
+function checkAndUploadFile() {
+    var tbFolderName = document.getElementById('mainContent_tbFileName');
+    if (tbFolderName == null) return;
+    if (tbFolderName.value == '') {
+        showMsgBox('Le nom du du fichier est vide !\r\nVous devez spécifier un nom de fichier.', 'ERREUR !', 'MsgBox');
+        return;
+    }
 
- 
+    // Ok upload file now...
+    var strFileName = tbFolderName.value;
+    var nIndex = strFileName.lastIndexOf('.');
+    if (nIndex < 0) {
+        showMsgBox('Le type de fichier inconnu !\r\nLes fichiers valables sont Word, Pdf, Excel, Images.', 'ERREUR !', 'MsgBox');
+        return;
+    }
 
-Step 1: Download Dropzone.js
+    var strExt = strFileName.substring(nIndex + 1).toUpperCase();
+    switch (strExt) {
+        case 'DOC':
+        case 'DOCX':
+            break;
 
- 
+        case 'XLS':
+        case 'XLSX':
+            break;
 
- Alternatively, you can use a CDN to load the library. Add the following code to your project’s layout page, preferably just before the closing “body” tag: “`
+        case 'PPT':
+        case 'PPTX':
+            break;
 
-<script src=”https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/min/dropzone.min.js”></script> <link rel=”stylesheet” href=”https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/min/dropzone.min.css” />
+        case 'PDF':
+            break;
 
-Step 2: Setup a Form Next, create a form in your view that will contain the Dropzone.js instance. You can use regular HTML form elements or use the ASP.NET MVC helper methods to create the form. For example:
+        case 'PNG':
+        case 'JPG':
+        case 'JPEG':
+        case 'GIF':
+        case 'BMP':
+            break;
 
-@using (Html.BeginForm(“Upload”, “Home”, FormMethod.Post, new { enctype = “multipart/form-data”, @class = “dropzone”, id = “file-upload-form” })) { <div class=”fallback”> <input type=”file” name=”file” /> </div> <button type=”submit” class=”btn btn-primary”>Upload</button> }
+        case 'XML':
+            break;
+        case 'BIN':
+            break;
+        case 'DWG':
+            break;
 
-Note that we are using the “dropzone” CSS class to define the form, which is important for Dropzone.js to work properly.
+        default:
+            showMsgBox('Le type de fichier [' + strExt + '] n\'est pas connu !\r\nLes fichiers valables sont Word, Pdf, Excel, Images.', 'ERREUR !', 'MsgBox');
+            return;
+    }
 
-Step 3: Configure Dropzone In the same view, add the following JavaScript code to configure Dropzone.js:
+    __doPostBack('uploadFile', '');
 
-<script type=”text/javascript”> $(document).ready(function () { Dropzone.autoDiscover = false; $(“#file-upload-form”).dropzone({ // Set any desired options for dropzone // For example, you can set upload URL and max file size url: ‘@Url.Action(“Upload”, “Home”)’, maxFilesize: 3, maxFiles: 10 }); });
-
-</script> “` In the above code, we have disabled automatic discovering of Dropzone instances, as we are manually initializing it on our form. Then we are providing a custom upload URL and setting the max file size and max files allowed.
-
-Step 4:
-
-MVC controller, create an action method to handle the file upload. For this example, we will create a method called “Upload” in the “HomeController”: “` [HttpPost] public ActionResult Upload(HttpPostedFileBase file) { if (file != null && file.ContentLength > 0) { var fileName = Path.GetFileName(file.FileName); var path = Path.Combine(Server.MapPath(“~/App_Data/uploads”), fileName); file.SaveAs(path); } return RedirectToAction(“Index”); }
-
-Step 5: Test the Upload Run your ASP.NET MVC application and browse to the view containing the form.
+}
