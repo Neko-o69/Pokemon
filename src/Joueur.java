6589ba37074fb77df6a3e5a527210f463ee31993
Neko-o69 <othmane.azizi05@gmail.com>
2026-06-04 09:46:59 +0200

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,29 +1,26 @@
-<tr>
-    <td></td>
-    <td>
-        <div style="min-width: 1024px;">
-            <table id="tableHeader" style="border-spacing: 0; border-collapse: collapse;">
-                <tr>
-                    <td>
-                        <asp:TextBox ID="tbMasterSearch" CssClass="tbSearch" Style="margin-left: 9px;"
-                            runat="server" placeholder="Recherche.." AutoPostBack="false" onkeydown="return onSearchKeyDown(event);"></asp:TextBox>
-                    </td>
-                    <td>
-                        <h1 style="margin-left: 5px; margin-right: 5px;">Base documentaire NIDEK</h1>
-                    </td>
-                    <td>
-                        <ul class="ulMenuHeaderBlue">
-                            <li><a href="javascript:onHome();">Accueil</a></li>
-                            <li><a href="QualityLogin.aspx">Se déconnecter</a></li>
-                            <%--<li><a href="javascript:onChangePassword();">Changer de mot de passe</a></li>--%>
-                        </ul>
-                    </td>
-                    <td>
-                        <asp:Label ID="lbUserName" runat="server" Text="Utilisateur" CssClass="labelMenuHeaderBlue"></asp:Label>
-                    </td>
-                </tr>
-            </table>
-        </div>
-    </td>
-</tr>
-<tr>
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=no" />
+
+    <link href="Content/Quality.css" rel="Stylesheet" type="text/css" />
+    <%--For Computer device--%>
+    <link rel="icon" href="ipad_nidek_interne.png" />
+    <link rel="shortcut icon" href="icon_nidek_interne.ico" />
+    <%--For mobile device--%>
+    <link rel="apple-touch-icon" href="ipad_nidek_interne.png" />
+    <link rel="apple-touch-icon-precomposed" href="icon_nidek_interne.png" />
+    <%--Indicate whether to hide the browser's status bar on startup:--%>
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <%--Indicate an image to display while the mobile web app is loading:--%>
+    <link rel="apple-touch-startup-image" href="ipad_nidek_interne.png" />
+
+    <script type="text/javascript" src="Scripts/QualityMaster.js"></script>
+
+    <title>NIDEK - Base documentaire</title>
+
+    <asp:ContentPlaceHolder ID="headContent" runat="server">
+    </asp:ContentPlaceHolder>
+
+</head>
