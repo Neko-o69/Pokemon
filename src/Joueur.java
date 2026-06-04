32a1d852fb270cd867a757b49d6a8e7e10be8954
Neko-o69 <othmane.azizi05@gmail.com>
2026-06-04 10:26:48 +0200

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,26 +1,4 @@
-<head runat="server">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, user-scalable=no" />
-
-    <link href="Content/Quality.css" rel="Stylesheet" type="text/css" />
-    <%--For Computer device--%>
-    <link rel="icon" href="ipad_nidek_interne.png" />
-    <link rel="shortcut icon" href="icon_nidek_interne.ico" />
-    <%--For mobile device--%>
-    <link rel="apple-touch-icon" href="ipad_nidek_interne.png" />
-    <link rel="apple-touch-icon-precomposed" href="icon_nidek_interne.png" />
-    <%--Indicate whether to hide the browser's status bar on startup:--%>
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <%--Indicate an image to display while the mobile web app is loading:--%>
-    <link rel="apple-touch-startup-image" href="ipad_nidek_interne.png" />
-
-    <script type="text/javascript" src="Scripts/QualityMaster.js"></script>
-
-    <title>NIDEK - Base documentaire</title>
-
-    <asp:ContentPlaceHolder ID="headContent" runat="server">
-    </asp:ContentPlaceHolder>
-
-</head>
+protected void UploadFile()
+{
+    ShowMsgBox("HasFile : " + DocFileUpload.HasFile.ToString() + " | FileName : " + DocFileUpload.FileName, "TEST", "MsgBox");
+    return; // retirer après le test
