18752b8041ae95b5d318c63b07e4475791b97a95
Neko-o69 <othmane.azizi05@gmail.com>
2026-06-04 09:34:28 +0200

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,59 +1,55 @@
 $(document).ready(function () {
     InitDragAndDrop();
-    DragAndDropOperation();
 });
+
 function InitDragAndDrop() {
-    $("#dropZone").on("dragenter", function (e) {
+    $("#dropZone").on("dragenter dragover", function (e) {
         e.preventDefault();
         e.stopPropagation();
+        $(this).css("background", "#cce");
     });
-    $("#dropZone").on("dragover", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
+
+    $("#dropZone").on("dragleave", function () {
+        $(this).css("background", "#f9f9f9");
     });
+
     $("#dropZone").on("drop", function (e) {
         e.preventDefault();
         e.stopPropagation();
-    });
-}
-function DragDropOperation() {
-    $("#dropZone").on("drop", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-    });
+        $(this).css("background", "#f9f9f9");
 
-    var files = e.originalEvent.dataTransfer.files;
-    var fileNames = "";
-    if (files.length > 0) {
-        filesName += "Uploading file <br />";
+        var files = e.originalEvent.dataTransfer.files;
+        if (files.length === 0) return;
 
+        var filesNames = "Upload en cours :<br />";
         for (var i = 0; i < files.length; i++) {
             filesNames += files[i].name + "<br />";
         }
-    }
-    $("#dropZone").html(filesNames);
-    var data = new FormData();
-    for (var i = 0; i < files.length; i++) {
-        data.append("file" + i, files[i]);
-    }
-    $.ajax({
-        type: "POST",
-        url: "QualityFolder.aspx/uploadFileDrop",
-        contentType: false,
-        processData: false,
-        data: data,
-        success: function (message) {
-            $("#dropZone").html(message);
-        },
-        error: function () {
-            $("#dropZone").html("Error Found");
-        },
-        beforeSend: function () {
-            $("#dropZone").show();
-        },
+        $("#dropZone").html(filesNames);
 
+        var data = new FormData();
+        for (var i = 0; i < files.length; i++) {
+            data.append("file" + i, files[i]);
+        }
+
+        var hfFolder = document.getElementById('mainContent_hfFolderTree');
+        data.append('__EVENTTARGET', 'uploadFileDrop');
+        data.append('__EVENTARGUMENT', '');
+        data.append('mainContent_hfFolderTree', hfFolder ? hfFolder.value : 'Quality');
+
+        $.ajax({
+            type: "POST",
+            url: "QualityFolder.aspx",
+            contentType: false,
+            processData: false,
+            data: data,
+            success: function () {
+                $("#dropZone").html("Glisser un fichier ici");
+                __doPostBack('gotoFolder', hfFolder ? hfFolder.value : '');
+            },
+            error: function () {
+                $("#dropZone").html("Erreur upload");
+            }
+        });
     });
-
-
-
-    }
+}
