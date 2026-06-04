a4de361564711a800475ab6f051cc73e56ad60e1
Neko-o69 <othmane.azizi05@gmail.com>
2026-06-04 11:34:53 +0200

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,28 +1,5 @@
-function InitDragAndDrop() {
-
-    $("#dropZone").on("dragover", function (e) {
-        e.preventDefault();
-        $(this).css("background", "#cce");
-    });
-
-    $("#dropZone").on("dragleave", function () {
-        $(this).css("background", "white");
-    });
-
-    $("#dropZone").on("drop", function (e) {
-        e.preventDefault();
-        $(this).css("background", "white");
-
-        var lesfichiers = e.originalEvent.dataTransfer.files;
-        if (lesfichiers.length == 0) return;
-
-        var monInput = document.getElementById('mainContent_DocFileUpload');
-        var transfer = new DataTransfer();
-        transfer.items.add(lesfichiers[0]);
-        monInput.files = transfer.files;
-
-        document.getElementById('mainContent_tbFileName').value = lesfichiers[0].name;
-
-        checkAndUploadFile();
-    });
+{
+    "Message": "Appel de service Web non valide, valeur manquante pour le paramètre : \u0027strData\u0027.",
+    "StackTrace": "   à System.Web.Script.Services.WebServiceMethodData.CallMethod(Object target, IDictionary`2 parameters)\r\n   à System.Web.Script.Services.RestHandler.InvokeMethod(HttpContext context, WebServiceMethodData methodData, IDictionary`2 rawParams)\r\n   à System.Web.Script.Services.RestHandler.ExecuteWebServiceCall(HttpContext context, WebServiceMethodData methodData)",
+    "ExceptionType": "System.InvalidOperationException"
 }
