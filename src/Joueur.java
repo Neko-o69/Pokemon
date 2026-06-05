552f284c4f52f95383bb0657970c0389ef21d506
Neko-o69 <othmane.azizi05@gmail.com>
2026-06-05 15:22:28 +0200

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,83 +1,63 @@
-[WebMethod(EnableSession = true)]
-public static string sendRequestModification(string strTo, string strObject, string strBody, string strAttach)
+public static string DeleteDir(string strFolder)
 {
-    String strUser, strPW, strServer, strAuthenfification, strXmlFile, strAttachFile, strEMail;
-    XmlDocument xmlFile = new XmlDocument();
-    XmlNodeList xlmItems;
-
-    if (HttpContext.Current.Session["User"] == null) return "ERROR ! User not login !";
-    NKUser nkUser = (NKUser)HttpContext.Current.Session["User"];
-    strXmlFile = HttpContext.Current.Server.MapPath("App_Data") + "\\Users\\" + nkUser.m_strCode + "\\SmtpServer.xml";
-    if (NKTools.IsFileExist(strXmlFile) == false) return "ERROR ! Configuration file for email not found ";
-    xmlFile.Load(strXmlFile);
-
-    xlmItems = xmlFile.GetElementsByTagName("Authentification");
-    strAuthenfification = xlmItems[0].InnerText.ToString().ToUpper();
-
-    xlmItems = xmlFile.GetElementsByTagName("User");
-    strUser = xlmItems[0].InnerText.ToString();
-
-    xlmItems = xmlFile.GetElementsByTagName("MW");
-    strPW = xlmItems[0].InnerText.ToString();
-
-    xlmItems = xmlFile.GetElementsByTagName("SMTP");
-    strServer = xlmItems[0].InnerText.ToString();
-
-
-    // Reformatter...
-    String strText = strBody.Replace('|', '\'');
-
-
-    ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP1);
-    service.Credentials = new WebCredentials(strUser, strPW, "NIDEK");
-    service.Url = new Uri( "https://mail.nidek.fr/EWS/Exchange.asmx" );
-    service.Url = new Uri(strServer);
-    EmailMessage message = new EmailMessage(service);
-    message.Subject = strObject;
-    //message.Body = MessageTextBox.Text;
-    message.Body = strText.Replace("\r\n", "<br/>");
-
-    //|*******************************************************************
-    //|-- Multi destiny
-    int nOffset = 0;
-    int nIndex = strTo.IndexOf(';');
-    while (nIndex > 0)
-    {
-        strEMail = strTo.Substring(nOffset, nIndex - nOffset).Trim();
-        if (String.IsNullOrEmpty(strEMail) == false)
-            message.ToRecipients.Add(strEMail);
-
-        nOffset += (nIndex - nOffset) + 1;
-        nIndex = strTo.IndexOf(';', nOffset);
-    }
-    // Last item;
-    strEMail = strTo.Substring(nOffset).Trim();
-    if (String.IsNullOrEmpty(strEMail) == false)
-        message.ToRecipients.Add(strEMail);
-    //|*******************************************************************
-
-    // Cc tjs à audrey
-    String strCC = ConfigurationManager.AppSettings["QualityManager"];
-    if (String.IsNullOrEmpty(strCC) == false)
-    {
-        message.CcRecipients.Add(strCC);
-    }
-
-    // Attachment
-    strAttachFile = HttpContext.Current.Server.MapPath("Images\\") + strAttach;
-    strAttachFile = strAttachFile.Replace("/", "\\");
-    if (NKTools.IsFileExist(strAttachFile) == false)
-        return "ERROR ! Attach file not exist";
-
-    message.Attachments.AddFileAttachment(strAttachFile);
+    String strRetCode;
+    String strSQL;
+    int nIndex;
+    String strFullPath;
+    String strConnection = ConfigurationManager.ConnectionStrings["OPHELIE"].ConnectionString;
     try
     {
-        message.Send();
+        DirectoryInfo dir = new DirectoryInfo(strFolder);
+        foreach (FileInfo fi in dir.GetFiles())
+        {
+            // Ok mise à jour dans la database        
+            nIndex = fi.FullName.IndexOf("\\Quality");
+            strFullPath = fi.FullName.Substring(nIndex + 1);
+            strFullPath = strFullPath.Replace('\\', '/');
+            strSQL = "delete from  BASE_DOCUMENTAIRES where BD_FULL_PATH = '" + strFullPath + "'";
+            if (DBHelper.SQLExecute(strSQL, strConnection) == false)
+                return "System error please contact admin for more details !";
+
+            fi.Delete();
+
+
+        }
+        foreach (DirectoryInfo di in dir.GetDirectories())
+        {
+            strRetCode = DeleteDir(di.FullName);
+            if (strRetCode != "true") return strRetCode;
+        }
+
+        // Finalement
+        dir.Delete();
+
     }
     catch (Exception ex)
     {
-        return "ERROR : " + ex.ToString();
+        return "ERROR : " + ex.Message;
     }
 
+    // Ok mise à jour dans la database
+    nIndex = strFolder.IndexOf("\\Quality");
+    strFullPath = strFolder.Substring(nIndex + 1);
+    strFullPath = strFullPath.Replace('\\', '/');
+    strSQL = "delete from  BASE_DOCUMENTAIRES where BD_FULL_PATH = '" + strFullPath + "'";
+    if (DBHelper.SQLExecute(strSQL, strConnection) == false)
+        return "System error please contact admin for more details !";
+
     return "true";
+}
+
+[WebMethod(EnableSession = true)]
+public static string deleteFolder(string strFullPath)
+{
+    String strPath = HttpContext.Current.Server.MapPath("Images\\") + strFullPath;
+    String strConnection = ConfigurationManager.ConnectionStrings["OPHELIE"].ConnectionString;
+    if (Directory.Exists(strPath))
+    {
+        DBHelper.SQLExecute("delete from BASE_DOCUMENTAIRES where BD_FULL_PATH LIKE '" + strFullPath + "'", strConnection);
+        return "true";
+    }
+
+        return DeleteDir(strPath);
 }
