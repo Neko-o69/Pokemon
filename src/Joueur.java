3f3c52acf380d4c680c7921eed5f52ec6a7acdce
Neko-o69 <othmane.azizi05@gmail.com>
2026-06-05 13:59:47 +0200

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,142 +1,94 @@
-protected void UploadFile()
-{
+<%@ Page Title="" Language="C#" MasterPageFile="~/Quality.master" AutoEventWireup="true" CodeFile="QualityProcess.aspx.cs" Inherits="QualityProcess" %>
 
-    NKUser nkUser = (NKUser)Session["User"];
-    if (nkUser.HasRight(NKUser.USER_RIGHT_UPLOAD) == false)
-    {
-        ShowMsgBox("Vous n'avez pas le droit pour cette opération", "ERREUR !", "MsgBox");
-        return;
-    }
-    if (DocFileUpload.HasFile == false) return;
+<asp:Content ID="head" ContentPlaceHolderID="headContent" runat="Server">
+</asp:Content>
+<asp:Content ID="menu" ContentPlaceHolderID="menuContent" runat="Server">
+</asp:Content>
+<asp:Content ID="main" ContentPlaceHolderID="mainContent" runat="Server">
 
-    // Save this file to temps folder
-    String strDate = DBHelper.GetCurrentDate();
-    String strParentFolder = hfFolderTree.Value;
-    if (String.IsNullOrEmpty(strParentFolder))
-        strParentFolder = "Quality";
-    String strFullPath = strParentFolder + "/" + DocFileUpload.FileName.Replace("'", "&quote");
-    String strFileName = Server.MapPath("Images/") + strFullPath;
+    <script src="Scripts/utility.js"></script>
+    <script src="Scripts/Quality.js"></script>
+    <script type="text/javascript">
 
-    int nIndex = DocFileUpload.FileName.LastIndexOf('.');
-    if (nIndex < 0) return;
-    String strExt = DocFileUpload.FileName.Substring(nIndex + 1).ToUpper();
-    String strDisplay = tbDisplay.Text;
-    if (strDisplay == "")
-        strDisplay = DocFileUpload.FileName.Substring(0, nIndex);
-    String strDescription = tbDescription.Text;
-
-    switch (strExt)
-    {
-        case "DOC":
-        case "DOCX":
-            nIndex = 1;
-            break;
-
-        case "XLS":
-        case "XLSX":
-            nIndex = 2;
-            break;
-
-        case "PDF":
-            nIndex = 3;
-            break;
-
-        case "PNG":
-        case "JPG":
-        case "JPEG":
-        case "GIF":
-        case "BMP":
-            nIndex = 4;
-            break;
-
-        case "PPT":
-        case "PPTX":
-            nIndex = 5;
-            break;
-
-        case "ZIP":
-        case "RAR":
-            nIndex = 6;
-            break;
-
-        case "MPP":
-            nIndex = 7;
-            break;
-
-        case "XML":
-            nIndex = 8;
-            break;
-        case "BIN":
-            nIndex = 9;
-            break;
-        case "DWG":
-            nIndex = 10;
-            break;
-
-        default:
-            ShowMsgBox("Le type de fichier [" + strExt + "] n'est pas connu !\r\nLes fichiers valables sont Word, Pdf, Excel, Images, Zip.", "ERREUR !", "MsgBox");
-            return;
-
-    }
-
-    try
-    {
-        FileInfo fi = new FileInfo(strFileName);
-        if (fi.Exists)
-        {
-            fi.Delete();
+        function onItemClicked(strSubFolder) {
+            var hf = document.getElementById('mainContent_hfProcess');
+            if (hf == null) return true;
+            var strFullPath = 'Quality/' + hf.value + '/' + strSubFolder;
+            window.location.href = 'QualityFolder.aspx?folder=' + strFullPath;
+            return false;
         }
-        DocFileUpload.SaveAs(strFileName);
 
-        // Save ok, insert into database
-        String strParent = strParentFolder;
-        //if (strParent == "Quality") strParent = ""; // Suis à la racine !
-        int nOrder = 0;
-        DataTable dt;
-        if (strParent == "")
-            dt = DBHelper.SQLOpen("select BD_ORDER from BASE_DOCUMENTAIRES " +
-                "where BD_FOLDER_PARENT is NULL or BD_FOLDER_PARENT = '' or BD_FOLDER_PARENT = 'Quality' order by BD_ORDER desc", m_strConnection);
-        else
-            dt = DBHelper.SQLOpen("select BD_ORDER from BASE_DOCUMENTAIRES where BD_FOLDER_PARENT = '" + strParent + "' order by BD_ORDER desc", m_strConnection);
+    </script>
 
-        if (dt.Rows.Count > 0)
-            nOrder = DBHelper.GetInt(0, "BD_ORDER", dt) + 1;
+    <asp:HiddenField ID="hfProcess" Value="AUT" runat="server" />
+
+    <%--Title --%>
+    <asp:Label ID="lbTitle" runat="server" CssClass="labelTitle" Text="Processus ???" Style="margin-left: 10px;"></asp:Label>
+    <div class="divLine"></div>
+
+    <table style="margin-top: 10px; margin-left: 250px; border-spacing: 0; border-collapse: collapse;">
+        <tr>
+            <td style="text-align: center;">
+                <a href="#">
+                    <asp:LinkButton ID="btnRef" runat="server" CssClass="divQualityPyramid" Style="background-image: url('../Images/icon_pyramid_1.png'); padding-top: 20px;"
+                        OnClientClick="return onItemClicked('REF');"
+                        Width="128px" Height="65px">
+                        Document de<br />référence-<br />REF</asp:LinkButton>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">
+                <asp:LinkButton ID="btnProc" runat="server" CssClass="divQualityPyramid" Style="background-image: url('../Images/icon_pyramid_2.png'); padding-top: 20px;"
+                    OnClientClick="return onItemClicked('PROC');" Width="244px" Height="65px">
+                    Procédure - <br />PROC</asp:LinkButton>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">
+                <asp:LinkButton ID="btnMOP" runat="server" CssClass="divQualityPyramid" Style="background-image: url('../Images/icon_pyramid_3.png'); padding-top: 30px;"
+                    OnClientClick="return onItemClicked('MOP');" Width="364px" Height="55px">
+                    Mode opératoire - MOP</asp:LinkButton>
+
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">
+                <asp:LinkButton ID="btnFORM" runat="server" CssClass="divQualityPyramid" Style="background-image: url('../Images/icon_pyramid_4.png'); padding-top: 30px;"
+                    OnClientClick="return onItemClicked('FORM');" Width="484px" Height="55px">
+                    Formulaires - FORM</asp:LinkButton>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">
+                <asp:LinkButton ID="btnDOC" runat="server" CssClass="divQualityPyramid" Style="background-image: url('../Images/icon_pyramid_5.png'); padding-top: 30px;"
+                    OnClientClick="return onItemClicked('DOC');" Width="604px" Height="160px">
+                    Documents - DOC</asp:LinkButton>
+            </td>
+            <td style="text-align:center;">
+                <%if (Request.QueryString["Type"] == "DRH")
+                    { %>
+                <asp:LinkButton ID="BtnACC" runat="server" CssClass="divQualityPyramid" Style="background-image: url('../Images/icon_pyramid_rond.png'); padding-top: 80px; text-align:center;  background-repeat: no-repeat; line-height:18px; background-size:200px 200px;"
+                    OnClientClick="return onItemClicked('ACC');" Width="200px" Height="160px">
+                    Accords d'entreprise<br />
+                    Chartes
+                </asp:LinkButton>
+                <% }  %>
+            </td>
+        </tr>
+
+        <%--<tr>
+            <td style="text-align:center;">
+                <asp:LinkButton ID="LinkButton1" runat="server" CssClass="divQualityPyramid" style="background-image:url('../Images/icon_pyramid_rond2.png');padding-top:30px;background-size:contain;"
+                    OnClientClick="return onItemClicked('DOC');" Width="120px" Height="55px">
+                    Documents - DOC</asp:LinkButton>
+            </td>
+        </tr>--%>
+    </table>
 
 
-        String strSQL = "insert into BASE_DOCUMENTAIRES" +
-            "(BD_FULL_PATH, BD_NAME, BD_DESCRIPTION, BD_FOLDER_PARENT, BD_CREATE_DATE, BD_CREATE_USER, BD_MODIFY_DATE, BD_MODIFY_USER, BD_TYPE, BD_ORDER) values (" +
-            "'" + DBHelper.CharToSQL(strFullPath) + "', '" + DBHelper.CharToSQL(strDisplay) + "', '" + DBHelper.CharToSQL(strDescription) + "', '" + strParent + "', " +
-            strDate + ", '" + nkUser.m_strCode + "', NULL, NULL, " + nIndex.ToString() + ", " + nOrder.ToString() + ")";
-        if (DBHelper.SQLExecute(strSQL, m_strConnection) == false)
-        {
-            fi.Delete();
-            ShowMsgBox("System error please contact admin for more details !", "ERROR !", "MsgBox");
-            return;
-        }
-        DataTable dtEmail = DBHelper.SQLOpen("select BD_EMAIL from BD_EMAIL where BD_FULL_PATH = '" + strParentFolder + "'", m_strConnection);
-        string strTo = "";
-        if (dtEmail.Rows.Count > 0)
-            strTo = dtEmail.Rows[0]["BD_EMAIL"].ToString();
-        if (String.IsNullOrEmpty(strTo)) strTo = "othmaneqlq@gmail.com"; // email par défaut
-        string result = sendRequestModification(strTo, strObject, strBody, strFullPath);
-        if (result != "true")
-            ShowMsgBox(result, "ERREUR MAIL", "MsgBox");
 
 
 
-        // Reload...
-        //gotoFolder(strParentFolder);
-        //if(dlFolder.Items.Count <= 0)
-        //{
-        //    hfAddNewFile.Value = "";
-        //    gotoFolder(strParentFolder);
-        //}
-        //else
-        //    hfAddNewFile.Value = strDisplay + "/" + nIndex.ToString() + "/" + strFullPath;
-    }
-    catch (Exception ex)
-    {
-        ShowMsgBox(ex.Message, "ERREUR SYSTEME !", "MsgBox");
-    }
-    
-}
+</asp:Content>
+
