fb311b8a12b7cb281f3876e9a869da0ffcbc56b8
Neko-o69 <othmane.azizi05@gmail.com>
2026-06-12 15:48:58 +0200

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,110 +1,111 @@
-function pageLoad() {
+function newName(name) {
+    if (name == "ACH")
+        return " Achat";
+    if (name == "MKT")
+        return " Marketing";
+    if (name == "ADV")
+        return " Administration Des Ventes";
+    if (name == "AUT")
+        return " Autre";
+    if (name == "DAF")
+        return "Finances";
+    if (name == "DRH")
+        return "Ressources Humaines";
+    if (name == "GEN")
+        return " Management";
+    if (name == "INF")
+        return " Informatique";
+    if (name == "LOG")
+        return " Logistique";
+    if (name == "TAB")
+        return " Site de fabrication";
+    if (name == "TEC")
+        return " Technique";
+    if (name == "VEN")
+        return " Vente";
+    return name;
+    if (name == "DOC")
+        return " Documents";
+    return name;
+    if (name == "FORM")
+        return " Formulaires";
+    return name;
+    if (name == "MOP")
+        return " Mode Opératoire";
+    return name;
+    if (name == "PROC")
+        return " Procédure";
+    return name;
+    if (name == "REF")
+        return " Référence";
+    return name;
+    
+    
+}
 
-    //showDragInfo();
 
+function populateAborescence() {
 
-    isIE = isIEBrowser();
-    isEDGE = isEDGEBowser();
+    var list = document.getElementById('ulAborescence');
+    if (list == null) return;
+    var hfFolder = document.getElementById('<%= hfFolderTree.ClientID %>');
+    if (hfFolder.value == null ||
+        hfFolder.value == "") return;
 
-    //|------------------------------------------------------
-    //|------------------------------------------------------
-    populateAborescence();
-
-    //|------------------------------------------------------
-    // Modify l'item btnDelete
-    //|------------------------------------------------------
-    var btn = document.getElementById('btnFolder');
-    if (btn) {
-        var hfCurrentFolder = document.getElementById('<%= hfCurrentFolder.ClientID %>');
-        if (hfCurrentFolder.value == 'Quality' || hfCurrentFolder.value == '') {
-            btn.text = 'Supprimer le dossier';
-            btn.style.display = 'none';
-            
-
-           
-           
+    var strFullPath = "";
+    var init = true;
+    var strFolder = hfFolder.value;
+    var nIndex = strFolder.indexOf('/');
+    while (nIndex >= 0) {
+        var strItem = strFolder.substring(0, nIndex);
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.href = "#"; // Default value
+        if (init) {
+            strFullPath = strItem;
+            init = false;
         }
         else {
-            btn.text = 'Supprimer le dossier : [' + hfCurrentFolder.value + ']';
-            btn.style.display = 'block';
+            strFullPath = strFullPath + "/" + strItem;
         }
-   
-       
+        if (strItem == 'Quality') {
+            a.text = '...';
+            a.href = "javascript:onItemClicked(null, 0, '')";
+        }
+        else {
+            a.text = newName(strItem);
+            a.href = "javascript:onItemClicked(null, 0, '" + strFullPath + "')";
+        }
+
+        li.appendChild(a);
+        list.appendChild(li);
+        strFolder = strFolder.substring(nIndex + 1);
+        nIndex = strFolder.indexOf('/');
+
     }
 
-    
- 
-    //|------------------------------------------------------
-    // Init Image viewer
-    //|------------------------------------------------------
-    osdViewer = OpenSeadragon({
-        id: "divImageViewer",
-        //prefixUrl: "/images/",
-        //tileSources: "/path/to/my/image.dzi"
-        tileSources: {
-            type: 'image',
-            url: '/Images/no_big_image.png'
+    // Last item
+    if (strFolder.length > 0) {
+        li = document.createElement("li");
+        var a = document.createElement("a");
+        a.href = "#";
+        if (init) {
+            strFullPath = strFolder;
+            init = false;
         }
-    });
-
-    //|------------------------------------------------------
-    // Check if add new file
-    //|------------------------------------------------------
-    var hfAddNewFile = document.getElementById('mainContent_hfAddNewFile');
-    if (hfAddNewFile && hfAddNewFile.value != "") {
-        // Décomposer 
-        var strName = "", strFullPath = "";
-        var nType = 1;
-        var nIndex = hfAddNewFile.value.indexOf('/');
-        if (nIndex > 0) {
-            strName = hfAddNewFile.value.substring(0, nIndex);
-            var strType = hfAddNewFile.value.substring(nIndex + 1, nIndex + 2);
-            nType = parseInt(strType);
-            strFullPath = hfAddNewFile.value.substring(nIndex + 3);
+        else {
+            strFullPath = strFullPath + "/" + strFolder;
         }
-
-        var dataList = document.getElementById('mainContent_dlFolder');
-        //var dataList = document.getElementById('mainContent_gvFolder');
-        if (dataList == null) return;
-
-        var row = dataList.insertRow(dataList.rows.length);
-        var td = row.insertCell(0);
-
-        var div = createDiv(strName, nType, strFullPath);
-        td.appendChild(div);
-
-        hfAddNewFile.value = "";
+        if (strFolder == 'Quality') {
+            a.text = '...';
+            a.href = "javascript:onItemClicked(null, 0, '')";
+        }
+        else {
+            a.text = newName(strFolder);
+            a.href = "javascript:onItemClicked(null, 0, '" + strFullPath + "')";
+        }
+        a.value ="";
+        li.appendChild(a);
+        list.appendChild(li);
     }
-
-    // Check user right
-    requestUserRight();
-
-
-    // select file ?
-    // this vient généralement de la page QualityQuery
-    var hfSelectedFile = document.getElementById('mainContent_hfSelectedFile');
-    if (hfSelectedFile == null) return;
-    if (hfSelectedFile.value.length <= 0) return;
-    var hfSelectedType = document.getElementById('mainContent_hfSelectedType');
-    if (hfSelectedType == null) return;
-    if (hfSelectedType.value.length <= 0) return;
-
-    hfSelectedRow = document.getElementById('mainContent_hfSelectedRow');
-    if (hfSelectedRow == null) return;
-    if (hfSelectedRow.value.length <= 0) return;
-
-    var dataList = document.getElementById('mainContent_dlFolder');
-    if (dataList == null) return;
-    var nRow = parseInt(hfSelectedRow.value);
-    if (nRow >= 0) {
-        var div = dataList.rows[nRow].children[0].children[0];
-        // Scroll first to d'abord
-        dataList.scrollTop = div.clientHeight * nRow;
-        var nType = parseInt(hfSelectedType.value);
-        if (nType > 0) {
-            onItemClicked(div, nType, hfSelectedFile.value);
-        }
-    }
-
-
 }
